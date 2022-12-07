727c1d6834185eb573f53372726b6b3f7bfe8dcb
chuyenmc <chuyenmc@icts.vn>
2022-12-07 19:57:41 +0700

    fix ui

--- src/components/DiaryDetailContent/DiaryDetailContent.tsx
+++ src/components/DiaryDetailContent/DiaryDetailContent.tsx
@@ -8,8 +8,10 @@ export default function DiaryDetailContent({ content }: IProps) {
   return (
     <div className="bg-mellow-apricot relative min-h-[1200px] z-[1] diaries-child-content">
       <div className="w-full mx-auto max-w-7xl">
-        <div className="absolute bg-corn-field rounded-[50px] text-slate-blue py-[50px] px-[70px] w-[90%] m-auto mt-[12%] mb-[20%] font-montserrat">
-          <PortableText value={content} />
+        <div className="flex justify-center">
+          <div className="absolute bg-corn-field rounded-[50px] text-slate-blue py-[50px] px-[70px] w-[90%] m-auto mt-[12%] mb-[20%] font-montserrat">
+            <PortableText value={content} />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Diaries/Diaries.tsx
+++ src/pages/Diaries/Diaries.tsx
@@ -16,7 +16,7 @@ export default function Diaries() {
           </h2>
         </div>
       </div>
-      <div className="bg-mellow-apricot min-h-[2700px] relative z-[1] diaries-items">
+      <div className="bg-mellow-apricot min-h-[2000px] relative z-[1] diaries-items">
         <div className="w-full mx-auto max-w-7xl">
           <div className="mb-[45%] pt-[10%]">
             <div className="flex flex-wrap items-center justify-start gap-8 m-auto xl:gap-16">
